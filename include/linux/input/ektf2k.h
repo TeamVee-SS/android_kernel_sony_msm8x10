8827b26edcf8e03c84fccdf9f86a06fc679c2ff6
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-11-16 10:53:08 -0200

    input: touch: ektf2k: Thanks Clang :)
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- include/linux/input/ektf2k.h
+++ include/linux/input/ektf2k.h
@@ -1,12 +1,12 @@
 #ifndef _LINUX_ELAN_KTF2K_H
 #define _LINUX_ELAN_KTF2K_H
 
-#define ELAN_X_MAX      1280
-#define ELAN_Y_MAX      2112
+#define ELAN_X_MAX 1280
+#define ELAN_Y_MAX 2112
 
 #define ELAN_KTF2K_NAME "elan-ktf2k-touch"
 
-struct elan_ktf2k_i2c_platform_data {	
+struct elan_ktf2k_i2c_platform_data {
 	uint16_t version;
 	int abs_x_min;
 	int abs_x_max;
@@ -19,9 +19,9 @@ struct elan_ktf2k_i2c_platform_data {
 	u32 hw_det_gpio_flags;
 	u32 reset_gpio_flags;
 	u32 intr_gpio_flags;
-	bool	i2c_pull_up;
+	bool i2c_pull_up;
 	//[Arima Edison] add--
-       int mode_check_gpio;
+	int mode_check_gpio;
 	int (*power)(int on);
 };
 
