414d50c945958fcd3d06e7168c763f8b1eea40ef
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-11-16 09:54:28 -0200

    media: video: nm32x: Thanks Clang :)
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- drivers/media/video/nmi/nm32x/nmi326_spi_drv.h
+++ drivers/media/video/nmi/nm32x/nmi326_spi_drv.h
@@ -16,43 +16,36 @@
 #define CONFIG_VIDEO_SPI_DEBUG
 #define NMI326_HW_CHIP_ID_CHECK
 
-
 /* Debug macro */
-#define SPI_DEBUG(fmt, ...)                                     \
-	do {                                                    \
-	        printk(                               \
-	                "%s: " fmt, __func__, ##__VA_ARGS__);   \
-	} while(0)
-
-
-#define SPI_WARN(fmt, ...)                                      \
-	do {                                                    \
-	        printk(KERN_WARNING                             \
-	                fmt, ##__VA_ARGS__);                    \
+#define SPI_DEBUG(fmt, ...)                                                    \
+	do {                                                                   \
+		printk("%s: " fmt, __func__, ##__VA_ARGS__);                   \
 	} while (0)
 
-#define SPI_ERROR(fmt, ...)                                     \
-	do {                                                    \
-	        printk(KERN_ERR                                 \
-	                "%s: " fmt, __func__, ##__VA_ARGS__);   \
-        } while (0)
+#define SPI_WARN(fmt, ...)                                                     \
+	do {                                                                   \
+		printk(KERN_WARNING fmt, ##__VA_ARGS__);                       \
+	} while (0)
 
-
+#define SPI_ERROR(fmt, ...)                                                    \
+	do {                                                                   \
+		printk(KERN_ERR "%s: " fmt, __func__, ##__VA_ARGS__);          \
+	} while (0)
 
 #ifdef CONFIG_VIDEO_SPI_DEBUG
-#define spi_dbg(fmt, ...)               SPI_DEBUG(fmt, ##__VA_ARGS__)
+#define spi_dbg(fmt, ...) SPI_DEBUG(fmt, ##__VA_ARGS__)
 #else
 #define spi_dbg(fmt, ...)
 #endif
 
-#define spi_warn(fmt, ...)              SPI_WARN(fmt, ##__VA_ARGS__)
-#define spi_err(fmt, ...)               SPI_ERROR(fmt, ##__VA_ARGS__)
+#define spi_warn(fmt, ...) SPI_WARN(fmt, ##__VA_ARGS__)
+#define spi_err(fmt, ...) SPI_ERROR(fmt, ##__VA_ARGS__)
 
 unsigned long check_dtv_chip_id(void);
 unsigned long nmi326_spi_read_chip_id(void);
-//int nmi326_spi_init(void);
-//void  nmi326_spi_exit( void );
+// int nmi326_spi_init(void);
+// void  nmi326_spi_exit( void );
 int nmi326_spi_read(u8 *buf, size_t len);
 int nmi326_spi_write(u8 *buf, size_t len);
 
-#endif	//__SPI_DRV_H
+#endif //__SPI_DRV_H
