f8c130a486870af95efd49e33e4813d72cba4b89
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-11-16 09:54:28 -0200

    media: video: nm32x: Thanks Clang :)
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- drivers/media/video/nmi/nm32x/nmi326.h
+++ drivers/media/video/nmi/nm32x/nmi326.h
@@ -17,8 +17,8 @@
 extern "C" {
 #endif
 
-#include <linux/ioctl.h>
 #include <linux/fs.h>
+#include <linux/ioctl.h>
 #include <linux/poll.h>
 #include <linux/sched.h>
 //#include "cust_gpio_usage.h"
@@ -33,44 +33,42 @@ extern "C" {
 #define NM_KMSG(x...) /* null */
 #endif
 
-#define ISDBT_DEV_NAME		"isdbt"  //isdbt
-#define ISDBT_DEV_MAJOR	227
-#define ISDBT_DEV_MINOR	0
+#define ISDBT_DEV_NAME "isdbt" // isdbt
+#define ISDBT_DEV_MAJOR 227
+#define ISDBT_DEV_MINOR 0
 
-#define DTV_IRQ_DEINIT			0
-#define DTV_IRQ_INIT			1
-#define DTV_IRQ_SET				2
-
+#define DTV_IRQ_DEINIT 0
+#define DTV_IRQ_INIT 1
+#define DTV_IRQ_SET 2
 
 typedef struct {
-	long		index;
-	void		**hInit;
-	void		*hI2C;
-	spinlock_t	isr_lock;
+	long index;
+	void **hInit;
+	void *hI2C;
+	spinlock_t isr_lock;
 	struct fasync_struct *async_queue;
-	unsigned char* rwBuf;
+	unsigned char *rwBuf;
 } ISDBT_OPEN_INFO_T;
 
-#define MAX_OPEN_NUM		8
+#define MAX_OPEN_NUM 8
 
-#define IOCTL_MAGIC	't'
+#define IOCTL_MAGIC 't'
 
-#define IOCTL_MAXNR				9
+#define IOCTL_MAXNR 9
 
-#define IOCTL_ISDBT_POWER_ON		_IO( IOCTL_MAGIC, 0 )
-#define IOCTL_ISDBT_POWER_OFF		_IO( IOCTL_MAGIC, 1 )
-#define IOCTL_ISDBT_RST_DN			_IO( IOCTL_MAGIC, 2 )
-#define IOCTL_ISDBT_RST_UP			_IO( IOCTL_MAGIC, 3 )
+#define IOCTL_ISDBT_POWER_ON _IO(IOCTL_MAGIC, 0)
+#define IOCTL_ISDBT_POWER_OFF _IO(IOCTL_MAGIC, 1)
+#define IOCTL_ISDBT_RST_DN _IO(IOCTL_MAGIC, 2)
+#define IOCTL_ISDBT_RST_UP _IO(IOCTL_MAGIC, 3)
 
-#define IOCTL_ISDBT_INTERRUPT_REGISTER		_IO(IOCTL_MAGIC, 4)
-#define IOCTL_ISDBT_INTERRUPT_UNREGISTER	_IO(IOCTL_MAGIC, 5)
-#define IOCTL_ISDBT_INTERRUPT_ENABLE		_IO(IOCTL_MAGIC, 6)
-#define IOCTL_ISDBT_INTERRUPT_DISABLE		_IO(IOCTL_MAGIC, 7)
-#define IOCTL_ISDBT_INTERRUPT_DONE		_IO(IOCTL_MAGIC, 8)
+#define IOCTL_ISDBT_INTERRUPT_REGISTER _IO(IOCTL_MAGIC, 4)
+#define IOCTL_ISDBT_INTERRUPT_UNREGISTER _IO(IOCTL_MAGIC, 5)
+#define IOCTL_ISDBT_INTERRUPT_ENABLE _IO(IOCTL_MAGIC, 6)
+#define IOCTL_ISDBT_INTERRUPT_DISABLE _IO(IOCTL_MAGIC, 7)
+#define IOCTL_ISDBT_INTERRUPT_DONE _IO(IOCTL_MAGIC, 8)
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif // __NMI_HW_H__
-
