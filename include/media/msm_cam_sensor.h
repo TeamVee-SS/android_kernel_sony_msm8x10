eb33e63f759d437d4a4034c0a4b7e61fd8cc2f3b
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-11-16 09:47:05 -0200

    msm: sensor: Add S5K5CA YUV Support
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -380,6 +380,8 @@ struct msm_sensor_init_params {
 	enum camb_position_t position;
 	/* sensor mount angle */
 	uint32_t            sensor_mount_angle;
+	/* sensor hawdware version */
+	int HW_VERSION;
 };
 
 struct msm_camera_sensor_slave_info {
@@ -486,6 +488,11 @@ enum msm_sensor_cfg_type_t {
 	CFG_SET_WHITE_BALANCE,
 	CFG_SET_AUTOFOCUS,
 	CFG_CANCEL_AUTOFOCUS,
+	CFG_SET_BRIGHTNESS,
+	CFG_GET_SHUTTER,
+	CFG_GET_GAIN,
+	CFG_GET_ISO,
+	CFG_SET_AUTO_BESTSHOT_MODE,
 };
 
 enum msm_actuator_cfg_type_t {
