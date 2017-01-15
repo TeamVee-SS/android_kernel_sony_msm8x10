137be12ea0eae558e1a92a6062bce2735551d464
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-15 20:50:12 -0200

    arm/dt: msm8x10: shuang: Updated code for 'test' branch
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- arch/arm/mach-msm/shuang/board-8610-shuang-gpiomux.c
+++ arch/arm/mach-msm/shuang/board-8610-shuang-gpiomux.c
@@ -25,6 +25,12 @@
 #define WLAN_DATA1	24
 #define WLAN_DATA2	23
 
+static struct gpiomux_setting gpio_2ma_pull_up = {
+	.func = GPIOMUX_FUNC_GPIO,
+	.drv = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_UP,
+};
+
 static struct gpiomux_setting gpio_spi_config = {
 	.func = GPIOMUX_FUNC_1,
 	.drv = GPIOMUX_DRV_6MA,
@@ -56,30 +62,6 @@ static struct gpiomux_setting gpio_nfc_config = {
 static struct gpiomux_setting gpio_nfc_sus_config = {
 	.func = GPIOMUX_FUNC_2,
 	.drv  = GPIOMUX_DRV_2MA,
-	.pull = GPIOMUX_PULL_DOWN,
-};
-
-static struct gpiomux_setting atmel_int_act_cfg = {
-	.func = GPIOMUX_FUNC_GPIO,
-	.drv = GPIOMUX_DRV_8MA,
-	.pull = GPIOMUX_PULL_UP,
-};
-
-static struct gpiomux_setting atmel_int_sus_cfg = {
-	.func = GPIOMUX_FUNC_GPIO,
-	.drv = GPIOMUX_DRV_2MA,
-	.pull = GPIOMUX_PULL_DOWN,
-};
-
-static struct gpiomux_setting atmel_reset_act_cfg = {
-	.func = GPIOMUX_FUNC_GPIO,
-	.drv = GPIOMUX_DRV_6MA,
-	.pull = GPIOMUX_PULL_UP,
-};
-
-static struct gpiomux_setting atmel_reset_sus_cfg = {
-	.func = GPIOMUX_FUNC_GPIO,
-	.drv = GPIOMUX_DRV_6MA,
 	.pull = GPIOMUX_PULL_DOWN,
 };
 
@@ -187,6 +169,27 @@ static struct gpiomux_setting gpio_int_sus_cfg = {
 
 static struct msm_gpiomux_config msm_gpio_int_configs[] __initdata = {
 	{
+		.gpio = 8,		/* TEMP_ALERT_N */
+		.settings = {
+			[GPIOMUX_ACTIVE]	= &gpio_int_act_cfg,
+			[GPIOMUX_SUSPENDED]	= &gpio_int_sus_cfg,
+		},
+	},
+	{
+		.gpio = 80,
+		.settings = {
+			[GPIOMUX_ACTIVE]	= &gpio_int_act_cfg,
+			[GPIOMUX_SUSPENDED]	= &gpio_int_sus_cfg,
+		},
+	},
+	{
+		.gpio = 81,
+		.settings = {
+			[GPIOMUX_ACTIVE]	= &gpio_int_act_cfg,
+			[GPIOMUX_SUSPENDED]	= &gpio_int_sus_cfg,
+		},
+	},
+	{
 		.gpio = 84,
 		.settings = {
 			[GPIOMUX_ACTIVE]	= &gpio_int_act_cfg,
@@ -215,6 +218,22 @@ static struct msm_gpiomux_config msm_lcd_configs[] __initdata = {
 		.settings = {
 			[GPIOMUX_ACTIVE]    = &lcd_te_act_config,
 			[GPIOMUX_SUSPENDED] = &lcd_te_sus_config,
+		},
+	},
+};
+
+static struct gpiomux_setting backlight_reset_cfg = {
+	.func = GPIOMUX_FUNC_GPIO,
+	.drv = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_NONE,
+	.dir = GPIOMUX_OUT_HIGH,
+};
+
+static struct msm_gpiomux_config msm_backlight_reset_configs[] __initdata = {
+	{
+		.gpio = 57,
+		.settings = {
+			[GPIOMUX_SUSPENDED] = &backlight_reset_cfg,
 		},
 	},
 };
@@ -296,20 +315,6 @@ static struct msm_gpiomux_config msm_nfc_configs[] __initdata = {
 
 
 static struct msm_gpiomux_config msm_atmel_configs[] __initdata = {
-	{
-		.gpio      = 0,		/* TOUCH RESET */
-		.settings = {
-			[GPIOMUX_ACTIVE] = &atmel_reset_act_cfg,
-			[GPIOMUX_SUSPENDED] = &atmel_reset_sus_cfg,
-		},
-	},
-	{
-		.gpio      = 1,		/* TOUCH INT */
-		.settings = {
-			[GPIOMUX_ACTIVE] = &atmel_int_act_cfg,
-			[GPIOMUX_SUSPENDED] = &atmel_int_sus_cfg,
-		},
-	},
 	{
 		.gpio      = 86,		/* BLSP1 QUP4 SPI_DATA_MOSI */
 		.settings = {
@@ -479,7 +484,7 @@ static struct gpiomux_setting gpio_suspend_config[] = {
 static struct gpiomux_setting cam_settings[] = {
 	{
 		.func = GPIOMUX_FUNC_1, /*active 1*/ /* 0 */
-		.drv = GPIOMUX_DRV_2MA,
+		.drv = GPIOMUX_DRV_6MA,
 		.pull = GPIOMUX_PULL_NONE,
 	},
 
@@ -540,10 +545,10 @@ static struct msm_gpiomux_config msm_sensor_configs[] __initdata = {
 		},
 	},
 	{
-		.gpio = 14, /* CAM_MCLK1 */
+		.gpio = 14, /* CAM_PWR_EN */
 		.settings = {
-			[GPIOMUX_ACTIVE]    = &cam_settings[0],
-			[GPIOMUX_SUSPENDED] = &cam_settings[1],
+			[GPIOMUX_ACTIVE]    = &cam_settings[3],
+			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
 		},
 	},
 	{
@@ -569,13 +574,6 @@ static struct msm_gpiomux_config msm_sensor_configs[] __initdata = {
 	},
 	{
 		.gpio = 19, /* FLASH_LED_NOW */
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &cam_settings[3],
-			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
-		},
-	},
-	{
-		.gpio = 8, /* CAM1_STANDBY_N */
 		.settings = {
 			[GPIOMUX_ACTIVE]    = &cam_settings[3],
 			[GPIOMUX_SUSPENDED] = &gpio_suspend_config[1],
@@ -723,6 +721,16 @@ static struct msm_gpiomux_config msm_cdc_dmic_configs[] __initdata = {
 	},
 };
 
+static struct msm_gpiomux_config peripheral_configs[] = {
+	{
+		.gpio = 84, /* HS_DET_N */
+		.settings = {
+			[GPIOMUX_ACTIVE]    = &gpio_2ma_pull_up,
+			[GPIOMUX_SUSPENDED] = &gpio_2ma_pull_up,
+		},
+	},
+};
+
 void __init msm8610_init_gpiomux(void)
 {
 	int rc;
@@ -734,6 +742,7 @@ void __init msm8610_init_gpiomux(void)
 	}
 
 	msm_gpiomux_install(msm_blsp_configs, ARRAY_SIZE(msm_blsp_configs));
+	msm_gpiomux_install(peripheral_configs, ARRAY_SIZE(peripheral_configs));
 	if (of_board_is_qrd()) {
 		msm_gpiomux_install(msm_focaltech_configs,
 			ARRAY_SIZE(msm_focaltech_configs));
@@ -745,6 +754,8 @@ void __init msm8610_init_gpiomux(void)
 			ARRAY_SIZE(wcnss_5wire_interface));
 	msm_gpiomux_install_nowrite(msm_lcd_configs,
 				ARRAY_SIZE(msm_lcd_configs));
+	msm_gpiomux_install(msm_backlight_reset_configs,
+				ARRAY_SIZE(msm_backlight_reset_configs));
 	msm_gpiomux_install(msm_keypad_configs,
 				ARRAY_SIZE(msm_keypad_configs));
 	msm_gpiomux_install(sd_card_det, ARRAY_SIZE(sd_card_det));
