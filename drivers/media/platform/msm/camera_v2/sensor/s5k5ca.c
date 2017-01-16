ad601a63bba3d02760f9e0994b276590984c9d49
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-16 00:26:10 -0200

    msm: camera: Multi module support in s5k5ca sensor architecture.
    * also bring back some code to fix building, until I not finish port
    
    * need to be fix before final release
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/s5k5ca.c
+++ drivers/media/platform/msm/camera_v2/sensor/s5k5ca.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -599,16 +599,17 @@ int32_t s5k5ca_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 	struct msm_sensor_power_setting_array *power_setting_array = NULL;
 	struct msm_sensor_power_setting *power_setting = NULL;
 	struct msm_camera_sensor_board_info *data = s_ctrl->sensordata;
-	struct gpio *gpio_req = data->gpio_conf->cam_gpio_req_tbl;
+	struct msm_camera_power_ctrl_t *power_info = &data->power_info;
+	struct msm_camera_gpio_conf *gpio_conf = power_info->gpio_conf;
+	struct gpio *gpio_req = gpio_conf->cam_gpio_req_tbl;
 
 	power_setting_array = &s_ctrl->power_setting_array;
 	power_setting = &power_setting_array->power_setting[index];
 
 	CDBG("peter: %s:%d\n", __func__, __LINE__);
 
-	rc = msm_camera_request_gpio_table(
-	    data->gpio_conf->cam_gpio_req_tbl,
-	    data->gpio_conf->cam_gpio_req_tbl_size, 1);
+	rc = msm_camera_request_gpio_table(gpio_conf->cam_gpio_req_tbl,
+					   gpio_conf->cam_gpio_req_tbl_size, 1);
 	if (rc < 0) {
 		pr_err("%s: request gpio failed\n", __func__);
 		return rc;
@@ -629,23 +630,17 @@ int32_t s5k5ca_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 
 	// mclk enable
 	if (power_setting->config_val) {
-		//		char *name=NULL;
-		//		name =
-		// kzalloc(sizeof(char)*strlen("cam_clk"),GFP_KERNEL);
-		//		memcpy(name,"cam_clk",sizeof(char)*strlen("cam_clk"));
-		//		s_ctrl->clk_info[power_setting->seq_val].clk_name
-		//= name;
-		s_ctrl->clk_info[power_setting->seq_val].delay =
+		power_info->clk_info[power_setting->seq_val].delay =
 		    power_setting->delay;
-		s_ctrl->clk_info[power_setting->seq_val].clk_rate =
+		power_info->clk_info[power_setting->seq_val].clk_rate =
 		    power_setting->config_val;
 	}
 	CDBG("peter: %s:%d clk_rate:%ld clk_info_size:%d", __func__, __LINE__,
-	     s_ctrl->clk_info[power_setting->seq_val].clk_rate,
-	     s_ctrl->clk_info_size);
-	rc = msm_cam_clk_enable(s_ctrl->dev, &s_ctrl->clk_info[0],
+	     power_info->clk_info[power_setting->seq_val].clk_rate,
+	     power_info->clk_info_size);
+	rc = msm_cam_clk_enable(power_info->dev, &power_info->clk_info[0],
 				(struct clk **)&power_setting->data[0],
-				s_ctrl->clk_info_size, 1);
+				power_info->clk_info_size, 1);
 	if (rc < 0) {
 		pr_err("%s: clk enable failed\n", __func__);
 	}
@@ -675,9 +670,9 @@ int32_t s5k5ca_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 	HW_VERSION_GPIO = gpio_req[5].gpio;
 
 	HW_VERSION = gpio_get_value(HW_VERSION_GPIO);
-	s_ctrl->sensordata->sensor_init_params->HW_VERSION = HW_VERSION;
-	pr_err("%s: HW_VERSION_GPIO=%d,HW_VERSION=%d\n", __func__,
-	       HW_VERSION_GPIO, HW_VERSION);
+	data->sensor_init_params->HW_VERSION = HW_VERSION;
+	pr_info("%s: HW_VERSION_GPIO = [%d], HW_VERSION = [%d]\n", __func__,
+		HW_VERSION_GPIO, HW_VERSION);
 
 	return rc;
 }
@@ -687,7 +682,9 @@ int32_t s5k5ca_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 	struct msm_sensor_power_setting_array *power_setting_array = NULL;
 	struct msm_sensor_power_setting *power_setting = NULL;
 	struct msm_camera_sensor_board_info *data = s_ctrl->sensordata;
-	struct gpio *gpio_req = data->gpio_conf->cam_gpio_req_tbl;
+	struct msm_camera_power_ctrl_t *power_info = &data->power_info;
+	struct msm_camera_gpio_conf *gpio_conf = power_info->gpio_conf;
+	struct gpio *gpio_req = gpio_conf->cam_gpio_req_tbl;
 
 	power_setting_array = &s_ctrl->power_setting_array;
 	power_setting = &power_setting_array->power_setting[index];
@@ -704,16 +701,15 @@ int32_t s5k5ca_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 	// 2.8 vreg enable
 	gpio_direction_output(gpio_req[2].gpio, 0);
 
-	msm_cam_clk_enable(s_ctrl->dev, &s_ctrl->clk_info[0],
+	msm_cam_clk_enable(power_info->dev, &power_info->clk_info[0],
 			   (struct clk **)&power_setting->data[0],
-			   s_ctrl->clk_info_size, 0);
+			   power_info->clk_info_size, 0);
 
 	if (data->i2c_conf && data->i2c_conf->use_i2c_mux)
 		s5k5ca_sensor_disable_i2c_mux(data->i2c_conf);
 
-	rc = msm_camera_request_gpio_table(
-	    data->gpio_conf->cam_gpio_req_tbl,
-	    data->gpio_conf->cam_gpio_req_tbl_size, 0);
+	rc = msm_camera_request_gpio_table(gpio_conf->cam_gpio_req_tbl,
+					   gpio_conf->cam_gpio_req_tbl_size, 0);
 	if (rc < 0) {
 		pr_err("%s: request gpio failed\n", __func__);
 		//		return rc;
@@ -807,6 +803,9 @@ int32_t s5k5ca_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		for (i = 0; i < SUB_MODULE_MAX; i++)
 			CDBG("%s:%d subdev_id[%d] %d\n", __func__, __LINE__, i,
 			     cdata->cfg.sensor_info.subdev_id[i]);
+		CDBG("%s:%d mount angle valid %d value %d\n", __func__,
+		     __LINE__, cdata->cfg.sensor_info.is_mount_angle_valid,
+		     cdata->cfg.sensor_info.sensor_mount_angle);
 
 		break;
 	case CFG_SET_INIT_SETTING:
@@ -977,10 +976,12 @@ int32_t s5k5ca_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		}
 		break;
 	case CFG_GET_SENSOR_INIT_PARAMS:
-		cdata->cfg.sensor_init_params =
-		    *s_ctrl->sensordata->sensor_init_params;
-		//		cdata->cfg.sensor_init_params.HW_VERSION
-		//=HW_VERSION;
+		cdata->cfg.sensor_init_params.modes_supported =
+		    s_ctrl->sensordata->sensor_info->modes_supported;
+		cdata->cfg.sensor_init_params.position =
+		    s_ctrl->sensordata->sensor_info->position;
+		cdata->cfg.sensor_init_params.sensor_mount_angle =
+		    s_ctrl->sensordata->sensor_info->sensor_mount_angle;
 		CDBG("%s:%d init params mode %d pos %d mount %d  HW_VERSION %d "
 		     "  \n",
 		     __func__, __LINE__,
@@ -991,7 +992,8 @@ int32_t s5k5ca_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		break;
 	case CFG_SET_SLAVE_INFO: {
 		struct msm_camera_sensor_slave_info sensor_slave_info;
-		struct msm_sensor_power_setting_array *power_setting_array;
+		struct msm_camera_power_ctrl_t *p_ctrl;
+		uint16_t size;
 		int slave_index = 0;
 		if (copy_from_user(
 			&sensor_slave_info, (void *)cdata->cfg.setting,
@@ -1006,35 +1008,33 @@ int32_t s5k5ca_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 			    sensor_slave_info.slave_addr >> 1;
 		}
 
-		/* Update sensor address type */
-		s_ctrl->sensor_i2c_client->addr_type =
-		    sensor_slave_info.addr_type;
-
 		/* Update power up / down sequence */
-		s_ctrl->power_setting_array =
-		    sensor_slave_info.power_setting_array;
-		power_setting_array = &s_ctrl->power_setting_array;
-		power_setting_array->power_setting =
-		    kzalloc(power_setting_array->size *
-				sizeof(struct msm_sensor_power_setting),
-			    GFP_KERNEL);
-		if (!power_setting_array->power_setting) {
-			pr_err("%s:%d failed\n", __func__, __LINE__);
-			rc = -ENOMEM;
-			break;
+		p_ctrl = &s_ctrl->sensordata->power_info;
+		size = sensor_slave_info.power_setting_array.size;
+		if (p_ctrl->power_setting_size < size) {
+			struct msm_sensor_power_setting *tmp;
+			tmp = kmalloc(sizeof(struct msm_sensor_power_setting) *
+					  size,
+				      GFP_KERNEL);
+			if (!tmp) {
+				pr_err("%s: failed to alloc mem\n", __func__);
+				rc = -ENOMEM;
+				break;
+			}
+			kfree(p_ctrl->power_setting);
+			p_ctrl->power_setting = tmp;
 		}
-		if (copy_from_user(
-			power_setting_array->power_setting,
-			(void *)
-			    sensor_slave_info.power_setting_array.power_setting,
-			power_setting_array->size *
-			    sizeof(struct msm_sensor_power_setting))) {
-			kfree(power_setting_array->power_setting);
+		p_ctrl->power_setting_size = size;
+
+		rc = copy_from_user(
+		    p_ctrl->power_setting,
+		    (void *)sensor_slave_info.power_setting_array.power_setting,
+		    size * sizeof(struct msm_sensor_power_setting));
+		if (rc) {
 			pr_err("%s:%d failed\n", __func__, __LINE__);
 			rc = -EFAULT;
 			break;
 		}
-		s_ctrl->free_power_setting = true;
 		CDBG("%s sensor id %x\n", __func__,
 		     sensor_slave_info.slave_addr);
 		CDBG("%s sensor addr type %d\n", __func__,
@@ -1043,20 +1043,15 @@ int32_t s5k5ca_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		     sensor_slave_info.sensor_id_info.sensor_id_reg_addr);
 		CDBG("%s sensor id %x\n", __func__,
 		     sensor_slave_info.sensor_id_info.sensor_id);
-		for (slave_index = 0; slave_index < power_setting_array->size;
+		for (slave_index = 0; slave_index < p_ctrl->power_setting_size;
 		     slave_index++) {
 			CDBG("%s i %d power setting %d %d %ld %d\n", __func__,
 			     slave_index,
-			     power_setting_array->power_setting[slave_index]
-				 .seq_type,
-			     power_setting_array->power_setting[slave_index]
-				 .seq_val,
-			     power_setting_array->power_setting[slave_index]
-				 .config_val,
-			     power_setting_array->power_setting[slave_index]
-				 .delay);
+			     p_ctrl->power_setting[slave_index].seq_type,
+			     p_ctrl->power_setting[slave_index].seq_val,
+			     p_ctrl->power_setting[slave_index].config_val,
+			     p_ctrl->power_setting[slave_index].delay);
 		}
-		kfree(power_setting_array->power_setting);
 		break;
 	}
 	case CFG_WRITE_I2C_ARRAY: {
@@ -1149,10 +1144,7 @@ int32_t s5k5ca_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		night_mode_capture_flag = 0;
 		night_mode_preview_flag = 0;
 		current_scene_flag = 0;
-		// pr_err("power up, hw_version =%d\n",
-		// s_ctrl->sensordata->sensor_init_params->HW_VERSION);
 		break;
-
 	case CFG_POWER_DOWN:
 
 		if (s_ctrl->func_tbl->sensor_power_down)
