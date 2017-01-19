3b39a08cb3fba9318a19340c635f578dfa313fb7
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-19 15:32:31 -0200

    msm: clock-8610: Disable unsupported camera's for FalconSS
    * fix "s5k5ca: probe of 6-0078 failed with error -107"
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- arch/arm/mach-msm/clock-8610.c
+++ arch/arm/mach-msm/clock-8610.c
@@ -3021,8 +3021,13 @@ static struct clk_lookup msm_clocks_8610[] = {
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-0001"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-007d"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006d"),
+#ifdef CONFIG_MACH_SONY_FALCONSS
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-0002"),
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-0078"),
+#else
 	CLK_LOOKUP("cam_src_clk", mclk1_clk_src.c, "6-0002"),
 	CLK_LOOKUP("cam_src_clk", mclk1_clk_src.c, "6-0078"),
+#endif
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-0020"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006a"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006f"),
@@ -3030,8 +3035,13 @@ static struct clk_lookup msm_clocks_8610[] = {
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-0001"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-007d"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006d"),
+#ifdef CONFIG_MACH_SONY_FALCONSS
+	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-0002"),
+	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-0078"),
+#else
 	CLK_LOOKUP("cam_clk", mclk1_clk.c, "6-0002"),
 	CLK_LOOKUP("cam_clk", mclk1_clk.c, "6-0078"),
+#endif
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-0020"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006a"),
 
