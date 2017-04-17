eb98c424fa8a260d26feffd33616c991452c6765
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-04-17 09:09:52 -0300

    msm: camera: Squashed revert "Multi module support in sensor architecture."
    Revert "ARM:dts:msm: update generic cam entries"
    
    This reverts commit 7eaf475ecdf649badc5cea5aa6c601ede5130a80.
    
    Revert "ARM: dts: msm: camera: Update generic cam entries"
    
    This reverts commit 88f9588a1bc2a639a639e8bf8c0950cdf337b116.
    
    Revert "ARM: dts: msm: Reorder sensor nodes"
    
    This reverts commit 5058b495165d06ced3923ca82136a82e052d1e26.
    
    Revert "ARM: dts : msm: Removed non-supported profiles from mtp,qrd,cdp"
    
    This reverts commit a2df86e06582ce5ebf05cdb8358e9cca2b485b54.
    
    Revert "ARM:dts:msm:8974 update generic cam entries"
    
    This reverts commit a133a3091448328335f78bdb9efeb01a679595c7.
    
    Revert "ARM: dts: msm: Add default angle, position and mode"
    
    This reverts commit f5455d81a49667b517f444bf05efccbe832cb168.
    
    Revert "ARM: dts: Add generic camera node for 8974MTP slot #2."
    
    This reverts commit c3c26ea8a6e085e57dc61fd2cdaa17b8ace934b4.
    
    Revert "ARM: dts: msm: camera: Add generic camera node per HW socket."
    
    This reverts commit e227e7eaccdee1d10ce43f5602b676e2f4094097.
    
    Revert "msm: camera: Don't return error code if array size is zero"
    
    This reverts commit 7a1b08afcf223d1839f6b8da2fa87727f50bd9d1.
    
    Revert "msm: camera: fix write sensor settings issue"
    
    This reverts commit 9020df3b49e1dae75f8f9812e1bf7fb462c3f22f.
    
    Revert "msm: camera: check size of allocated buffer to prevent overflow"
    
    This reverts commit 9ea308044eb3ccc88b392227276d9a74ccac364e.
    
    Revert "msm-camera: Update sensor mount angle and position"
    
    This reverts commit 61c641f92254bbd0804b22b8d23daebad480efcc.
    
    Revert "msm: camera: sensor: sanitizing user space pointer"
    
    This reverts commit ea2eb1b1cfbfbdb58ddf0851d3ffa68ef80ed621.
    
    Revert "msm: camera: Populate sensor actuator subdev id during probe"
    
    This reverts commit 88af496ba73b078c3eb2a3d5c6ad0d86afdbcb54.
    
    Revert "msm: camera: Populate sensor eeprom subdev id during probe"
    
    This reverts commit 7a80b8f2f724b625bd93ad8bde4616b3f9174d56.
    
    Revert "msm: camera: Removed CCI_RELEASE call."
    
    This reverts commit bbae949d43ec1104e78776b06235c79ae89fc469.
    
    Revert "msm: camera: shut down master clock for eeprom in power down"
    
    This reverts commit 8af292548ed47fb81153a5bdec20fa42b4d59bfb.
    
    Revert "msm: camera: Fix for camera powerup crash"
    
    This reverts commit 194ba365d580bdac05c7b8ba47ba1144099f235c.
    
    Revert "msm: camera: Add power down sequence separately for camera"
    
    This reverts commit 1bdb7cdb3576e1170684aba3757eb24fa8149aca.
    
    Revert "msm: camera: Fix for gpio usage in camera modules"
    
    This reverts commit 8234aebbc9669379c0f940417c808fcf47b38e64.
    
    Revert "msm: camera: Updated the vreg parameters for powerdown."
    
    This reverts commit 131a9186a1b602e8204d291389420d77d4d782f1.
    
    Revert "msm: camera: Camera multimodule fixes for 8x10 platform."
    
    This reverts commit d9ca993e06d62446db48b140943deacb0feeae17.
    
    Revert "msm-camera: Use waiteventinterruptibletimeout during sensor init."
    
    This reverts commit a56ce9ce4084390e21575b9d218c89ffa6d7e62d.
    
    Revert "msm: camera: sensor: remove unnecessary logs"
    
    This reverts commit e897c00d4c80a9dedbffb79ce6088ed2a7571b0a.
    
    Revert "msm: camera: check size of allocated buffer to prevent overflow"
    
    This reverts commit 4867cee6f82acfa1f3d3a1e1cde2f95abbc225c6.
    
    Revert "msm: camera: Fix mt9m114 sensor orientation"
    
    This reverts commit 04af3f86a4f78c9c2404a071a7141e5e8aeb577c.
    
    Revert "msm: camera: Multi module support in sensor architecture."
    
    This reverts commit 11d85b028d1424875a4f5ad7aef0dd61f894ca29.

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -13,8 +13,6 @@
 #define I2C_REG_DATA_MAX       (8*1024)
 #define MAX_CID                   16
 
-#define I2C_USER_REG_DATA_MAX 1024
-
 #define MSM_SENSOR_MCLK_8HZ   8000000
 #define MSM_SENSOR_MCLK_16HZ  16000000
 #define MSM_SENSOR_MCLK_24HZ  24000000
@@ -55,7 +53,6 @@
 
 #define MAX_AF_ITERATIONS 3
 #define MAX_NUMBER_OF_STEPS 47
-#define MAX_POWER_CONFIG 12
 
 typedef enum sensor_stats_type {
 	YRGB,
@@ -107,8 +104,6 @@ enum msm_sensor_power_seq_gpio_t {
 	SENSOR_GPIO_VANA,
 	SENSOR_GPIO_VDIG,
 	SENSOR_GPIO_VAF,
-	SENSOR_GPIO_FL_EN,
-	SENSOR_GPIO_FL_NOW,
 	SENSOR_GPIO_MAX,
 };
 
@@ -236,8 +231,6 @@ struct msm_sensor_power_setting {
 struct msm_sensor_power_setting_array {
 	struct msm_sensor_power_setting *power_setting;
 	uint16_t size;
-	struct msm_sensor_power_setting *power_down_setting;
-	uint16_t size_down;
 };
 
 struct msm_sensor_id_info_t {
@@ -245,20 +238,12 @@ struct msm_sensor_id_info_t {
 	uint16_t sensor_id;
 };
 
-enum msm_sensor_camera_id_t {
-	CAMERA_0,
-	CAMERA_1,
-	CAMERA_2,
-	CAMERA_3,
-	MAX_CAMERAS,
+struct msm_camera_sensor_slave_info {
+	uint16_t slave_addr;
+	enum msm_camera_i2c_reg_addr_type addr_type;
+	struct msm_sensor_id_info_t sensor_id_info;
+	struct msm_sensor_power_setting_array power_setting_array;
 };
-
-enum cci_i2c_master_t {
-	MASTER_0,
-	MASTER_1,
-	MASTER_MAX,
-};
-
 
 struct msm_camera_i2c_reg_array {
 	uint16_t reg_addr;
@@ -343,20 +328,10 @@ struct csi_lane_params_t {
 	uint8_t csi_phy_sel;
 };
 
-enum camb_position_t {
-	BACK_CAMERA_B,
-	FRONT_CAMERA_B,
-	INVALID_CAMERA_B,
-};
-
 struct msm_sensor_info_t {
-	char     sensor_name[MAX_SENSOR_NAME];
-	int32_t  session_id;
-	int32_t  subdev_id[SUB_MODULE_MAX];
-	uint8_t  is_mount_angle_valid;
-	uint32_t sensor_mount_angle;
-	int modes_supported;
-	enum camb_position_t position;
+	char sensor_name[MAX_SENSOR_NAME];
+	int32_t    session_id;
+	int32_t     subdev_id[SUB_MODULE_MAX];
 };
 
 struct camera_vreg_t {
@@ -368,10 +343,14 @@ struct camera_vreg_t {
 	uint32_t delay;
 };
 
+enum camb_position_t {
+	BACK_CAMERA_B,
+	FRONT_CAMERA_B,
+};
+
 enum camerab_mode_t {
 	CAMERA_MODE_2D_B = (1<<0),
-	CAMERA_MODE_3D_B = (1<<1),
-	CAMERA_MODE_INVALID = (1<<2),
+	CAMERA_MODE_3D_B = (1<<1)
 };
 
 struct msm_sensor_init_params {
@@ -385,19 +364,6 @@ struct msm_sensor_init_params {
 	/* sensor hawdware version */
 	int HW_VERSION;
 #endif
-};
-
-struct msm_camera_sensor_slave_info {
-	char sensor_name[32];
-	char eeprom_name[32];
-	char actuator_name[32];
-	enum msm_sensor_camera_id_t camera_id;
-	uint16_t slave_addr;
-	enum msm_camera_i2c_reg_addr_type addr_type;
-	struct msm_sensor_id_info_t sensor_id_info;
-	struct msm_sensor_power_setting_array power_setting_array;
-	uint8_t  is_init_params_valid;
-	struct msm_sensor_init_params sensor_init_params;
 };
 
 struct sensorb_cfg_data {
@@ -660,20 +626,6 @@ struct msm_camera_led_cfg_t {
 	uint32_t flash_current[2];
 };
 
-/* sensor init structures and enums */
-enum msm_sensor_init_cfg_type_t {
-	CFG_SINIT_PROBE,
-	CFG_SINIT_PROBE_DONE,
-	CFG_SINIT_PROBE_WAIT_DONE,
-};
-
-struct sensor_init_cfg_data {
-	enum msm_sensor_init_cfg_type_t cfgtype;
-	union {
-		void *setting;
-	} cfg;
-};
-
 #define VIDIOC_MSM_SENSOR_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 1, struct sensorb_cfg_data)
 
@@ -700,9 +652,6 @@ struct sensor_init_cfg_data {
 
 #define VIDIOC_MSM_SENSOR_GET_AF_STATUS \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 9, uint32_t)
-
-#define VIDIOC_MSM_SENSOR_INIT_CFG \
-	_IOWR('V', BASE_VIDIOC_PRIVATE + 10, struct sensor_init_cfg_data)
 
 #define MSM_V4L2_PIX_FMT_META v4l2_fourcc('M', 'E', 'T', 'A') /* META */
 
