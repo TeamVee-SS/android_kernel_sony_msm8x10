fb2a8a9d3a92b6a79f5abb9f2b60fde346efcf71
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-05-12 11:18:18 -0300

    power: Remove non-working leftover code
    --
    From: =?UTF-8?q?Arve=20Hj=C3=B8nnev=C3=A5g?= <arve@android.com>
    Date: Tue, 5 Jun 2012 18:06:25 -0700
    Subject: [PATCH] Revert "PM: Add user-space wake lock api."
    
    This reverts commit 025ff0ad2ff6030b7071fc3da7dda4a96ec91468.
    --
    From: =?UTF-8?q?Arve=20Hj=C3=B8nnev=C3=A5g?= <arve@android.com>
    Date: Tue, 5 Jun 2012 18:06:42 -0700
    Subject: [PATCH] Revert "power: Add option to log time spent in suspend"
    
    This reverts commit 847bb6f2bc6e7c6d9c8e8a450b586a8ca3f1dba5.
    --
    
    Change-Id: I79d489f3690b114938bf2b40ea0e0f1edfd43444
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -601,11 +601,6 @@ power_attr(pm_trace_dev_match);
 
 #endif /* CONFIG_PM_TRACE */
 
-#ifdef CONFIG_USER_WAKELOCK
-power_attr(wake_lock);
-power_attr(wake_unlock);
-#endif
-
 static struct attribute *g[] = {
 	&state_attr.attr,
 #ifdef CONFIG_PM_TRACE
@@ -626,10 +621,6 @@ static struct attribute *g[] = {
 	&touch_event_timer_attr.attr,
 #ifdef CONFIG_PM_DEBUG
 	&pm_test_attr.attr,
-#endif
-#ifdef CONFIG_USER_WAKELOCK
-	&wake_lock_attr.attr,
-	&wake_unlock_attr.attr,
 #endif
 #endif
 	NULL,
