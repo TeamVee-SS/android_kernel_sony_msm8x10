c7737e7b90f15c2b57aefc56a7cdb32d4f993514
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-06-12 15:45:28 -0300

    Revert "camera: isp: create debug fs entry to log vfe overflows"
    This reverts commit eaa822ac8e6cd1f062d5427f5b4ca318c038111f.

--- drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
@@ -479,74 +479,44 @@ static void msm_vfe40_process_error_status(struct vfe_device *vfe_dev)
 		pr_err_ratelimited("%s: violation\n", __func__);
 		msm_vfe40_process_violation_status(vfe_dev);
 	}
-	if (error_status1 & (1 << 9)) {
-		vfe_dev->stats->imagemaster0_overflow++;
+	if (error_status1 & (1 << 9))
 		pr_err_ratelimited("%s: image master 0 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 10)) {
-		vfe_dev->stats->imagemaster1_overflow++;
+	if (error_status1 & (1 << 10))
 		pr_err_ratelimited("%s: image master 1 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 11)) {
-		vfe_dev->stats->imagemaster2_overflow++;
+	if (error_status1 & (1 << 11))
 		pr_err_ratelimited("%s: image master 2 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 12)) {
-		vfe_dev->stats->imagemaster3_overflow++;
+	if (error_status1 & (1 << 12))
 		pr_err_ratelimited("%s: image master 3 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 13)) {
-		vfe_dev->stats->imagemaster4_overflow++;
+	if (error_status1 & (1 << 13))
 		pr_err_ratelimited("%s: image master 4 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 14)) {
-		vfe_dev->stats->imagemaster5_overflow++;
+	if (error_status1 & (1 << 14))
 		pr_err_ratelimited("%s: image master 5 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 15)) {
-		vfe_dev->stats->imagemaster6_overflow++;
+	if (error_status1 & (1 << 15))
 		pr_err_ratelimited("%s: image master 6 bus overflow\n",
 			__func__);
-	}
-	if (error_status1 & (1 << 16)) {
-		vfe_dev->stats->be_overflow++;
+	if (error_status1 & (1 << 16))
 		pr_err_ratelimited("%s: status be bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 17)) {
-		vfe_dev->stats->bg_overflow++;
+	if (error_status1 & (1 << 17))
 		pr_err_ratelimited("%s: status bg bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 18)) {
-		vfe_dev->stats->bf_overflow++;
+	if (error_status1 & (1 << 18))
 		pr_err_ratelimited("%s: status bf bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 19)) {
-		vfe_dev->stats->awb_overflow++;
+	if (error_status1 & (1 << 19))
 		pr_err_ratelimited("%s: status awb bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 20)) {
-		vfe_dev->stats->imagemaster0_overflow++;
+	if (error_status1 & (1 << 20))
 		pr_err_ratelimited("%s: status rs bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 21)) {
-		vfe_dev->stats->cs_overflow++;
+	if (error_status1 & (1 << 21))
 		pr_err_ratelimited("%s: status cs bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 22)) {
-		vfe_dev->stats->ihist_overflow++;
+	if (error_status1 & (1 << 22))
 		pr_err_ratelimited("%s: status ihist bus overflow\n", __func__);
-	}
-	if (error_status1 & (1 << 23)) {
-		vfe_dev->stats->skinbhist_overflow++;
+	if (error_status1 & (1 << 23))
 		pr_err_ratelimited("%s: status skin bhist bus overflow\n",
 			__func__);
-	}
 }
 
 static void msm_vfe40_read_irq_status(struct vfe_device *vfe_dev,
