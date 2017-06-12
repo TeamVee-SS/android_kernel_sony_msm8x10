8db1aa3f51d6f98a12d6b0996326959ba8ca4072
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-06-12 15:27:19 -0300

    Revert "msm: camera: Reset HALT reg after the halt."
    This reverts commit 7b9f4554e62247ec50e2bd96d48d55d9f8161432.

--- drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
@@ -368,9 +368,6 @@ static void msm_vfe40_process_reset_irq(struct vfe_device *vfe_dev,
 static void msm_vfe40_process_halt_irq(struct vfe_device *vfe_dev,
 	uint32_t irq_status0, uint32_t irq_status1)
 {
-    if (irq_status1 & (1 << 8)) {
-        msm_camera_io_w(0x0, vfe_dev->vfe_base + 0x2C0);
-    }
 }
 
 static void msm_vfe40_process_camif_irq(struct vfe_device *vfe_dev,
@@ -1204,6 +1201,7 @@ static long msm_vfe40_axi_halt(struct vfe_device *vfe_dev,
 	msm_camera_io_w(0xFEFFFEFF, vfe_dev->vfe_base + 0x34);
 	msm_camera_io_w(0x1, vfe_dev->vfe_base + 0x24);
 	if (blocking) {
+		init_completion(&vfe_dev->halt_complete);
 		/* Halt AXI Bus Bridge */
 		msm_camera_io_w_mb(0x1, vfe_dev->vfe_base + 0x2C0);
 		atomic_set(&vfe_dev->error_info.overflow_state, NO_OVERFLOW);
@@ -1213,9 +1211,7 @@ static long msm_vfe40_axi_halt(struct vfe_device *vfe_dev,
 				axi_busy_flag = false;
 		}
 	}
-        else {
-	        msm_camera_io_w_mb(0x1, vfe_dev->vfe_base + 0x2C0);
-        }
+	msm_camera_io_w_mb(0x0, vfe_dev->vfe_base + 0x2C0);
 	return rc;
 }
 
